75704e5cc3940bf6b6e62b9deefe377eeb88db17
Gunavanth Reddy <gunavanthreddy220@gmail.com>
2026-07-24 15:58:34 +0530

    Added Restaurant App

---
+++ Restaurent App/script/data.js
@@ -0,0 +1,53 @@
+const menuItems = [
+    {
+        name: "Chicken 65",
+        price: 220,
+        category: "Starter"
+    },
+    {
+        name: "Chicken Lollipop",
+        price: 250,
+        category: "Starter"
+    },
+    {
+        name: "Chicken Biryani",
+        price: 300,
+        category: "Main Course"
+    },
+    {
+        name: "Mutton Biryani",
+        price: 380,
+        category: "Main Course"
+    },
+    {
+        name: "Gulab Jamun",
+        price: 100,
+        category: "Dessert"
+    },
+    {
+        name: "Chocolate Ice Cream",
+        price: 120,
+        category: "Ice Cream"
+    }
+];
+
+const tables = [
+    {
+        name: "Table 1",
+        orders: []
+    },
+    {
+        name: "Table 2",
+        orders: []
+    },
+    {
+        name: "Table 3",
+        orders: []
+    },
+    {
+        name: "Table 4",
+        orders: []
+    }
+];
+
+let selectedTable = null;
