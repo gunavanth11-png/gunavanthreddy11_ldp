f3692716f71efaea9957bbf43c5dd05c8b4ef58a
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-07-24 15:29:13 +0530

    Refactor email fetching to use dynamic user ID

--- AsyncAwait assignment/assignment.js
+++ AsyncAwait assignment/assignment.js
@@ -2,18 +2,16 @@ const getData = (uId) => {
     return new Promise((resolve) => {
         setTimeout(() => {
             console.log("Fetched the data!");
-            resolve("gmail@gmail.com");
+            resolve(`User ${uId}: skc@gmail.com`);
         }, 4000);
     });
 };
 
-const displayEmail = async () => {
+const main = async () => {
     console.log("start");
-
-    const email = await getData("gmail");
-
-    console.log(`Email id of the user id is: ${email}`);
+    const email = await getData("skc");
+    console.log("Email id of the user id is: " + email);
     console.log("end");
 };
 
-displayEmail();
+main();
