dd8b126734f62be85c69b6a567bbd15b1a0842ce
Gunavanth Reddy <gunavanthreddy220@gmail.com>
2026-07-27 14:56:32 +0530

    Add atomic design components

---
+++ component-assignment/component-assignment/src/components/App.tsx
@@ -0,0 +1,7 @@
+import Header from "./components/organisms/Header";
+
+const App = () => {
+    return <Header />;
+};
+
+export default App;
---
+++ component-assignment/component-assignment/src/components/organisms/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <h1>Atomic Design Assignment</h1>;
+};
+
+export default Header;
