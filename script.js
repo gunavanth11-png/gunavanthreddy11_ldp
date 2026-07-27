891ba776d16bf2ec23718b0ebf826323eab30472
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-07-27 12:31:49 +0530

    Create script.js
    I divided html and js files separate

---
+++ script.js
@@ -0,0 +1,19 @@
+let localScore =
+    Number(localStorage.getItem("localScore")) || 0;
+
+let sessionScore =
+    Number(sessionStorage.getItem("sessionScore")) || 0;
+
+document.getElementById("localScore").innerText = localScore;
+document.getElementById("sessionScore").innerText = sessionScore;
+
+function incrementScore() {
+    localScore++;
+    sessionScore++;
+
+    localStorage.setItem("localScore", localScore);
+    sessionStorage.setItem("sessionScore", sessionScore);
+
+    document.getElementById("localScore").innerText = localScore;
+    document.getElementById("sessionScore").innerText = sessionScore;
+}
