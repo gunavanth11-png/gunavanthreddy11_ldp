f954f1cbb29cbf21edfb5baa1978f4c838c5997c
Gunavanth Reddy <gunavanthreddy220@gmail.com>
2026-07-29 13:22:02 +0530

    Updated component assignment structure

---
+++ component-assignment/component-assignment/src/components/atoms/Button/index.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+}
+
+const Button = ({ text, onClick }: ButtonProps) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
