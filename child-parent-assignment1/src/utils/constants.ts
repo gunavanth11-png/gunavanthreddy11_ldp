1dc3985ed7a1e00bb715524fa980be6f3c8f491f
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-10 18:03:37 +0530

    Update constants.ts

--- child-parent-assignment1/src/utils/constants.ts
+++ child-parent-assignment1/src/utils/constants.ts
@@ -1,1 +1,17 @@
+import home from "../assets/icons/home.svg";
+import users from "../assets/icons/users.svg";
+import gavel from "../assets/icons/gavel.svg";
+import document from "../assets/icons/document.svg";
+import chart from "../assets/icons/chart.svg";
+import account from "../assets/icons/account.svg";
+import screening from "../assets/icons/screening.svg";
 
+export const SIDEBAR_MENU_OPTIONS = [
+  { text: "Home", icon: home, active: false },
+  { text: "Candidates", icon: users, active: true },
+  { text: "Adverse Actions", icon: gavel, active: false },
+  { text: "Logs", icon: document, active: false },
+  { text: "Analytics", icon: chart, active: false },
+  { text: "Account", icon: account, active: false },
+  { text: "Screenings", icon: screening, active: false },
+];
