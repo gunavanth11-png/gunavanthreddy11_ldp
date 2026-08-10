cda48fb54e7286d36ac0cb59c3c4f670e4a4d210
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-10 17:43:19 +0530

    Update ProfileCard.tsx

--- child-parent-assignment1/src/components/molecules/ProfileCard/ProfileCard.tsx
+++ child-parent-assignment1/src/components/molecules/ProfileCard/ProfileCard.tsx
@@ -1,1 +1,29 @@
+import Icon from "../../atoms/Icon/Icon";
+import "./ProfileCard.css";
 
+interface ProfileCardProps {
+  image: string;
+  name: string;
+  company: string;
+}
+
+const ProfileCard = ({ image, name, company }: ProfileCardProps) => {
+  return (
+    <div className="profile-card">
+      <Icon
+        src={image}
+        alt={name}
+        width="38px"
+        height="38px"
+        borderRadius="50%"
+      />
+
+      <div>
+        <div className="profile-card-name">{name}</div>
+        <div className="profile-card-company">{company}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
