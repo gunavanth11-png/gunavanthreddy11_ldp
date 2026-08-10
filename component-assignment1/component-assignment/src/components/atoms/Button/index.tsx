ad774c54e50f3efea9e80a3f7599dfa1ee2bad38
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-10 15:33:26 +0530

    Update index.tsx

--- component-assignment1/component-assignment/src/components/atoms/Button/index.tsx
+++ component-assignment1/component-assignment/src/components/atoms/Button/index.tsx
@@ -1,21 +1,12 @@
-interface ButtonProps {
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
 }
 
-const Button = ({ title }: ButtonProps) => {
+const Button = ({ title, ...props }: ButtonProps) => {
   return (
-    <button
-      style={{
-        backgroundColor: "#6C63FF",
-        color: "#fff",
-        border: "none",
-        padding: "12px 24px",
-        borderRadius: "8px",
-        cursor: "pointer",
-        fontWeight: 600,
-        fontSize: "15px",
-      }}
-    >
+    <button {...props}>
       {title}
     </button>
   );
