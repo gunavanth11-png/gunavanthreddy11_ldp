39fc3de056ef5a126c0434fa8ccf2dd58e10f06a
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-10 15:36:08 +0530

    Update App.tsx

--- component-assignment1/component-assignment/src/App.tsx
+++ component-assignment1/component-assignment/src/App.tsx
@@ -1,33 +1,30 @@
 import { FaHome, FaCalendarAlt } from "react-icons/fa";
+import "./App.css";
 
 import SidebarItem from "./components/molecules/SidebarItem";
 import StatCard from "./components/molecules/StatCard";
-
 import Button from "./components/atoms/Button";
 
 function App() {
   return (
-    <div
-      style={{
-        padding: "40px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "30px",
-        fontFamily: "Arial",
-      }}
-    >
+    <div className="app">
       <SidebarItem
         icon={<FaHome />}
         title="Home"
+        className="sidebar-item"
       />
 
       <StatCard
         icon={<FaCalendarAlt />}
         title="Term Cap"
         value="12 Months"
+        className="stat-card"
       />
 
-      <Button title="New Cash Kick" />
+      <Button
+        title="New Cash Kick"
+        className="app-button"
+      />
     </div>
   );
 }
