c5f167f3f9da10dde7021ea626093c2bb5f5bfed
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-10 15:35:25 +0530

    Update index.tsx

--- component-assignment1/component-assignment/src/components/molecules/StatCard/index.tsx
+++ component-assignment1/component-assignment/src/components/molecules/StatCard/index.tsx
@@ -1,42 +1,37 @@
-import { ReactNode } from "react";
+import { ReactNode, CSSProperties } from "react";
 import Typography from "../../atoms/Typography";
 
 interface StatCardProps {
   icon: ReactNode;
   title: string;
   value: string;
+  className?: string;
+  style?: CSSProperties;
+  titleStyle?: CSSProperties;
+  valueStyle?: CSSProperties;
 }
 
 const StatCard = ({
   icon,
   title,
   value,
+  className,
+  style,
+  titleStyle,
+  valueStyle,
 }: StatCardProps) => {
   return (
-    <div
-      style={{
-        width: "220px",
-        padding: "20px",
-        borderRadius: "12px",
-        border: "1px solid #E5E7EB",
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-      }}
-    >
-      <div style={{ fontSize: "24px" }}>
-        {icon}
-      </div>
+    <div className={className} style={style}>
+      {icon}
 
       <Typography
         text={title}
-        color="#777"
+        style={titleStyle}
       />
 
       <Typography
         text={value}
-        size="24px"
-        weight={700}
+        style={valueStyle}
       />
     </div>
   );
