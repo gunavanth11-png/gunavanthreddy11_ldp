48658cc29bac65cb26bec45dad70ca087452d701
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-08-24 12:09:03 +0530

    Update index.tsx

--- story-assignment1/storybook-assignment1/src/components/atoms/Button/index.tsx
+++ story-assignment1/storybook-assignment1/src/components/atoms/Button/index.tsx
@@ -1,26 +1,15 @@
-interface ButtonProps {
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
-  onClick?: () => void;
 }
 
-const Button = ({ title, onClick }: ButtonProps) => {
+const Button = ({ title, ...props }: ButtonProps) => {
   return (
-    <button
-      onClick={onClick}
-      style={{
-        backgroundColor: "#6C5CE7",
-        color: "#fff",
-        border: "none",
-        borderRadius: "8px",
-        padding: "12px 20px",
-        cursor: "pointer",
-        fontSize: "16px",
-        fontWeight: 600,
-      }}
-    >
+    <button {...props}>
       {title}
     </button>
   );
 };
 
-export default Button;
+export default Button; 
