29157364f25344920b187d6386e1ad5b75de12e1
gunavanth11-png <gunavanthreddy220@gmail.com>
2026-09-02 12:19:26 +0530

    Delete solidprinciples/src/solidprinciples directory

--- solidprinciples/src/solidprinciples/single/EmployeeReport.java
+++
@@ -1,9 +0,0 @@
-package solidprinciples.single;
-
-public class EmployeeReport {
-
-    public void generateReport(Employee employee) {
-        System.out.println("Employee Name: " + employee.getName());
-        System.out.println("Employee Salary: " + employee.getSalary());
-    }
-}
