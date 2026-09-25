108fc4662a9eaf5ac7852b775c535e3239569b03
Gunavanth Reddy <gunavanthreddy220@gmail.com>
2026-09-25 17:10:39 +0530

    feat: initial commit for maven-assignment1 project

---
+++ src/main/java/com/example/MavenAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenAssignmentApplication.class, args);
+    }
+}
